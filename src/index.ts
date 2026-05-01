ebf8541ea585551825c6981c1640514044686a51
aledma74 <aledma74@naver.com>
2026-05-02 01:05:18 +0900

    mcp server 테스트 완료

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import z from "zod";
+import { createMcpHandler } from "agents/mcp";
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Bye Bye! Hello World!");
+		const server = new McpServer({
+			name: "Stocks Server",
+			version: "1.0",
+		});
+		server.registerTool("get-stock-price", {
+			description: "티커 심볼을 입력받아 해당 주식의 가격을 조회한다.",
+			inputSchema: {
+				symbol: z.string(),
+			},
+		},
+		async ({symbol}) => {
+			return {
+				content: [
+					{
+						type: "text",
+						text: `${symbol}의 가격은 $10 USD 입니다.`,
+					}
+				]
+			}
+		});
+
+		const handler = createMcpHandler(server);
+		
+		return handler(request, env, ctx);
 	},
 } satisfies ExportedHandler<Env>;
